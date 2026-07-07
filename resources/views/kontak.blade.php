d1f43eec659166920b4258adad5ae3589d87079f
falahfansa6-commits <falahfansa6@gmail.com>
2026-07-07 08:18:33 +0700

    panggilan navbar

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -33,24 +33,11 @@
   <!-- ========================= -->
 <!-- NAVBAR MIRIP GAMBAR -->
 
-    <nav class="navbar">
-        
-        <a href="#" class="logo-container">
-            <img src="{{ asset('img/printex-merah.png') }}" alt="Logo" class="logo-img">
-        </a>
-        <div class="search-container">
-             <form action="{{ route('search') }}" method="GET">
-    <input type="text" name="keyword" class="search-input"placeholder="Cari...">
-             </div>
-        <div class="menu-container">
-            <a href="{{ route('beranda') }}" class="menu-item">Beranda</a>
-            <a href="{{ route('about') }}" class="menu-item">Tentang</a>
-            <a href="{{ route('pelayanan') }}" class="menu-item">Layanan</a>
-            <a href="{{ route('kontak') }}" class="menu-item">Kontak</a>
-            
-            <button class="btn-utama">Masuk atau Daftar</button>
-        </div>
+    <nav >
+        @include('layouts.navbar')
     </nav>
+
+    
     <div class="hk-section-wrapper">
         <div class="hk-badge">Kontak Kami</div>
         <h2 class="hk-main-title">Hubungi <span class="hk-title-underline">Kami</span></h2>
--- resources/views/pelayanan.blade.php
+++ resources/views/pelayanan.blade.php
@@ -57,25 +57,8 @@
   <!-- ========================= -->
 <!-- NAVBAR MIRIP GAMBAR -->
 
-    <nav class="navbar">
-        
-        <a href="#" class="logo-container">
-            <img src="printex-merah.png" alt="Logo" class="logo-img">
-        </a>
-        <div class="search-container">
-           <form action="{{ route('search') }}" method="GET">
-    <input type="text" name="keyword" class="search-input"placeholder="Cari...">
-</form>
-        </div>
-
-        <div class="menu-container">
-            <a href="{{ route('beranda') }}" class="menu-item">Beranda</a>
-            <a href="{{ route('about') }}" class="menu-item">Tentang</a>
-            <a href="{{ route('pelayanan') }}" class="menu-item">Layanan</a>
-            <a href="{{ route('kontak') }}" class="menu-item">Kontak</a>
-            
-            <button class="btn-utama">Masuk atau Daftar</button>
-        </div>
+     <nav >
+        @include('layouts.navbar')
     </nav>
   <!-- SLIDER -->
     <section class="slider-container">
